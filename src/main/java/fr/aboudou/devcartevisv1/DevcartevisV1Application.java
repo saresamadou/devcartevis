329c3855877232d3b946a3f8cb4c9e7784291707
sare <saresamadou1991@gmail.cpm>
2018-03-24 11:22:00 +0100

    devcarteis: mise en place du squelette de la première version

---
+++ src/main/java/fr/aboudou/devcartevisv1/DevcartevisV1Application.java
@@ -0,0 +1,12 @@
+package fr.aboudou.devcartevisv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevcartevisV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevcartevisV1Application.class, args);
+	}
+}
